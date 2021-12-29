e134d06ae091e353707b92b0eef3cc00aa87ad8e
Borislav Markov <borkox@gmail.com>
2021-12-29 19:59:41 +0200

    Changed reasoner

--- src/main/java/bg/unisofia/bsmarkov/ont/CriminalInference.java
+++ src/main/java/bg/unisofia/bsmarkov/ont/CriminalInference.java
@@ -4,15 +4,12 @@ import static bg.unisofia.bsmarkov.ont.CriminalStory.NS;
 import static org.apache.jena.ontology.OntModelSpec.OWL_DL_MEM_RULE_INF;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
-import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.reasoner.Reasoner;
-import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
+import org.apache.jena.reasoner.rulesys.BasicForwardRuleReasoner;
 import org.apache.jena.reasoner.rulesys.Rule;
 
 public class CriminalInference {
@@ -23,7 +20,7 @@ public class CriminalInference {
     public static OntModel makeDeductions(OntModel inputOnt) throws IOException {
         List<Rule> rules = Utils.readRules();
         // Create reasoner based on our custom rules
-        Reasoner reasoner = new GenericRuleReasoner(rules);
+        Reasoner reasoner = new BasicForwardRuleReasoner(rules);
 
         InfModel inf2 = ModelFactory.createInfModel(reasoner, inputOnt);
 
