1cb0ae6832c89242e5dcfb1170ffdd35b78dd7ee
Borislav Markov <borkox@gmail.com>
2021-12-29 22:52:40 +0200

    Fixing rules and removing RDF/XML

--- src/main/java/bg/unisofia/bsmarkov/ont/App.java
+++ src/main/java/bg/unisofia/bsmarkov/ont/App.java
@@ -10,7 +10,6 @@ public class App {
         Utils.saveOntology("gen-ontology/soap-ontology-initial.ttl", "TURTLE", ont1);
         OntModel ont2 = CriminalInference.makeDeductions(ont1);
         Utils.saveOntology("gen-ontology/soap-ontology-inference.ttl", "TURTLE", ont2);
-        Utils.saveOntology("gen-ontology/soap-ontology-inference.xml", "RDF/XML", ont2);
 
         CriminalDeduction.criminalDeduction(ont2);
     }
